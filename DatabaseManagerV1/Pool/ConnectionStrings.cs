53c21f724af9175a2ed514e682ba41154e92155d
REFUPANKER <68808212+REFUPANKER@users.noreply.github.com>
2024-02-05 21:15:35 +0300

    Update ConnectionStrings.cs

--- DatabaseManagerV1/Pool/ConnectionStrings.cs
+++ DatabaseManagerV1/Pool/ConnectionStrings.cs
@@ -3,7 +3,7 @@ namespace DatabaseManagerV1.Pool
 {
 	public static class ConnectionStrings
 	{
-		public static string Core_CodeSharingApp { get => $"server={Environment.MachineName};database=Test1;Trusted_Connection=True;"; }
-		public static string Core_Hotel { get => $"server=localhost;uid=root;password=12345;database=t4sch"; }
+		public static string Core_CodeSharingApp { get => $"ConnectionString"; }
+		public static string Core_Hotel { get => $"ConnectionString"; }
 	}
 }
